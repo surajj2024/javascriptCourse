35da95b4d025092a3223c9dc58cf10a2506f87be
surajj2024 <surajjkumar2024@gmail.com>
2024-02-24 01:23:55 +0530

    decision making

--- firstDay.js
+++ firstDay.js
@@ -1,2 +1,7 @@
-let hero = 0 / 0;
-console.log(hero);
+// dies
+let die1 = Math.floor(Math.random() * 6) + 1;
+let die2 = Math.floor(Math.random() * 6) + 1;
+
+let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}`;
+console.log(roll);
+
