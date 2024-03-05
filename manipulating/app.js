a2d3b88599d3cda82a2e09f6e9ffd4129f7562c8
surajj2024 <surajjkumar2024@gmail.com>
2024-03-06 02:14:07 +0530

    feat: :heavy_plus_sign: Done with the append and all things

--- manipulating/app.js
+++ manipulating/app.js
@@ -1,12 +1,46 @@
-const allLinks = document.querySelectorAll('a');
+/* let images = document.getElementsByTagName('img');
 
-// for (let link of allLinks) {
-//     link.innerText = 'I AM A LINK!!!!'
-// }
+for (const img of images) {
+    img.src =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg";
+} */
+/* 
+let sources = document.querySelector('.square');
+let link = sources.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+ */
+
+/* const h2 = document.querySelector("h2");
+h2.classList.add("purple");
+h2.classList.add("border");
+// h2.classList.remove("border");
+h2.classList.toggle("border");
+ */
+
+// create and append new element
+/* const newImg = document.createElement("img");
+newImg.src =
+  "https://images.unsplash.com/photo-1707343845208-a20c56d2c8ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8";
+
+document.body.appendChild(newImg);
+newImg.classList.add("border", "square");
+
+const newH3 = document.createElement("h3");
+newH3.innerHTML = "<b>New tags</b>"
+document.body.appendChild(newH3); */
+//NOTE -  append, prepend, insertAdjacentElement, after, before
+
+// removeChild, remove
+
+/* fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+        const newImage = document.createElement("img");
+        newImage.src = data.message; // Accessing the 'message' property of the returned object
+        document.body.appendChild(newImage);
+    })
+    .catch((error) => {
+        console.error("Error fetching image:", error);
+    }); */
 
 
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-}
+    
